e90d793cd8a5328b9886dac2adaab3354535fee5
Kyle Christie <55036011+kc101010@users.noreply.github.com>
2020-12-09 21:06:43 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup, Extension
 
 #usage: 
-#       `Python3 setup.py build` to create folder w/ library
-#       `Python3 setup.py install` to install library to system
+#       `Python3 setup.py build`        to create folder w/ library
+#       `Python3 setup.py install`      to install library to system
 
 module = Extension("deviewer", sources = ["deviewer.c"])
 
-setup(name="deviewer-test",
-            version = "0.1",
+setup(name="deviewer-dev",
+            version = "0.3",
             description="Package for deviewer",
             ext_modules = [module])
